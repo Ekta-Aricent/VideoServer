ee140f983a7910cf927b616cf1fd6b3bbdc9b81c
Ekta-Aricent <36473855+Ekta-Aricent@users.noreply.github.com>
2018-02-14 17:21:34 +0530

    init File

---
+++ init.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    nohup python /opt/dtedge/VideoServer/server.py > /var/log/Videoserver.log 2>&1 &
+    ;;
+  stop)
+    killall python
+    ;;
+  *)
+    echo "Usage: /etc/init.d/start_app.sh {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
